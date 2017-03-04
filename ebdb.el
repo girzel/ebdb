2b28f572c599caa93d4744faac2a4c574d2874d2
Eric Abrahamsen <eric@ericabrahamsen.net>
2017-03-03 16:12:15 -0800

    Bump to version 0.2
    * ebdb.el: Just because.

--- ebdb.el
+++ ebdb.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2016-2017  Free Software Foundation, Inc.
 
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((cl-lib "0.5") (seq "2.15"))
 
 ;; Maintainer: Eric Abrahamsen <eric@ericabrahamsen.net>
