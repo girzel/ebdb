05761c67f4890c37fb4e3a89c4bb8100f131df08
Eric Abrahamsen <eric@ericabrahamsen.net>
2017-03-17 12:25:10 -0700

    WIP on importing vCards

--- ebdb-vcard.el
+++ ebdb-vcard.el
@@ -119,8 +119,17 @@ to classify properties during import.  The import process does
 not always respect these headings."
 
   :group 'ebdb-vcard
-  :type '(repeat
-	  (cons string symbol)))
+  :type '(alist :key-type string :value-type symbol))
+
+(defcustom ebdb-vcard-kind-class-alist
+  '(("individual" . ebdb-record-person)
+    ("org" . ebdb-record-organization))
+  "Alist holding correspondences between vCard 4.0 KIND property
+  values and EBDB record classes."
+  :group 'ebdb-vcard
+  :type '(alist :key-type string :value-type symbol))
+
+(define-error 'ebdb-unparseable-vcard "vCard version not supported" 'ebdb-unparseable)
 
 (defsubst ebdb-vcard-escape (str)
   "Escape commas, semi-colons and newlines in STR."
@@ -308,14 +317,17 @@ method is just responsible for formatting the record name."
 (cl-defmethod ebdb-fmt-field ((_f ebdb-formatter-vcard)
 			      (name ebdb-field-name-complex)
 			      _style
-			      _record)
-  (with-slots (surname given-names prefix suffix) name
-    (format
-     "%s;%s;%s;%s"
-     (or surname "")
-     (if given-names (mapconcat #'identity given-names ","))
-     (or prefix "")
-     (or suffix ""))))
+			      (record ebdb-record))
+  (let ((sort-as (slot-value (ebdb-record-cache record) 'sortkey)))
+   (with-slots (surname given-names prefix suffix) name
+     (format
+      "SORT-AS;:%s;%s;%s;%s;%s"
+      sort-as
+      (or surname "")
+      (if given-names (car given-names) "")
+      (if (cdr given-names) (mapconcat #'identity (cdr given-names) ",") "")
+      (or prefix "")
+      (or suffix "")))))
 
 (cl-defmethod ebdb-fmt-field-label ((_f ebdb-formatter-vcard)
 				    (_field ebdb-field-uuid)
@@ -394,6 +406,298 @@ method is just responsible for formatting the record name."
 	       (calendar-gregorian-from-absolute
 		(slot-value ann 'date))))
     (format "%d%02d%02d" year month day)))
+
+;; Parsing vCard
+
+(defun ebdb-vcard-parse-file (file load &optional noerror)
+  "Parse FILE as a *.vcf vCard file.
+
+See `ebdb-vcard-parse-buffer' for the meaning of optional
+arguments LOAD and NOERROR."
+  (interactive
+   (list (read-file-name "Load contacts from file: " nil nil t)
+	 (ebdb-prompt-for-db)))
+  ;; Don't do any checking of FILE, just read it and assume it's
+  ;; vCard.  Raise errors if necessary.
+  (with-temp-buffer
+    (insert-file-contents file)
+    (ebdb-vcard-parse-buffer load noerror)))
+
+(defun ebdb-vcard-parse-buffer (&optional load noerror)
+  "Parse the current buffer as a vCard file.
+
+Look for individual record boundaries and pass chunks of text to
+`ebdb-vcard--dispatch', which passes the text to the appropriate
+`ebdb-parse-record' method.
+
+The return value is a list of EBDB record instances.  If optional
+argument LOAD is given, the records are also loaded into a
+database.  LOAD can be t, in which case the user may be prompted
+for a database to use, or it can be an actual database instance.
+
+By default, unparseable records will raise an error.  If optional
+argument NOERROR is t, those records will be silently discarded.
+If it is the symbol 'message, print a message after parsing about
+the number of unusable records.  If it is the symbol 'buffer, pop
+up a buffer containing the unparseable text."
+  (interactive)
+  (let ((case-fold-search t)
+	(unparseable (cl-case noerror
+		       (buffer
+			"Unparseable records:\n\n")
+		       (message
+			0)
+		       (t nil)))
+	records chunk)
+    (goto-char (point-min))
+    (while (re-search-forward "begin:vcard\n" (point-max) t)
+
+      (setq chunk
+	    (buffer-substring
+	     (point)
+	     (save-excursion
+	       (re-search-forward "end:vcard\n" (point-max))
+	       (forward-line -1)
+	       (point))))
+
+      (condition-case err
+	  (push
+	   (ebdb-vcard--dispatch chunk)
+	   records)
+	(ebdb-unparseable-vcard
+	 (cond
+	  ((eql noerror 'buffer)
+	   (setq unparseable (concat unparseable "\n\n" chunk)))
+	  ((eql noerror 'message)
+	   (cl-incf unparseable))
+	  (noerror
+	   nil)
+	  (t (signal 'ebdb-unparseable-vcard (list err)))))))
+    
+    (when load
+      (let ((db (cond ((and (eieio-object-p load)
+			    (object-of-class-p load ebdb-db))
+		       load)
+		      ((= 1 (length ebdb-db-list))
+		       (car ebdb-db-list)
+		       (t
+			(ebdb-prompt-for-db))))))
+	(dolist (r records)
+	  (ebdb-db-add-record db r))
+	(message "Imported %d records to %S"
+		 (length records)
+		 db)))
+    
+    (when unparseable
+      (cond ((eql noerror 'message)
+	     (message "%d unparseable vCard records ignored" unparseable))
+	    ((eql noerror 'buffer)
+	     (pop-to-buffer
+	      (get-buffer-create "*EBDB Parse Errors*")
+	      '(nil . ((window-height . 10))))
+	     (insert unparseable)
+	     (fit-window-to-buffer)
+	     (goto-char (point-min)))
+	    ;; We shouldn't get here; at this stage NOERROR should
+	    ;; only be one of the above symbols.
+	    (t nil)))
+
+    records))
+
+(defun ebdb-vcard--dispatch (text)
+  "Dispatches TEXT (a vCard) to the appropriate `ebdb-parse-record' method.
+
+TEXT should be the text of a vCard contact _without_ the BEGIN
+and END lines.  This function assumes the first line will contain
+a VERSION property, and uses that and other information to choose
+an appropriate vCard formatter to use in parsing, or else signals
+`ebdb-unparseable-vcard'.
+
+Returns the results of the call to `ebdb-parse-record', with the
+expectation that that will be a `ebdb-record' class instance.
+Does not handle any `ebdb-unparseable-vcard' errors raised in
+that method."
+  (with-temp-buffer
+    (insert text)
+    (goto-char (point-min))
+    ;; Right now, we just choose between 3.0 and 4.0.  Later, we'll
+    ;; check for Apple/Google/Microsoft-specific vCard flavors, and
+    ;; handle those here, too.
+    (let* ((version (aref (ebdb-vcard-extract-property "version") 2))
+	   (fmt (cond
+		 ((string= version "3.0")
+		  ebdb-vcard-default-30-formatter)
+		 ((string= version "4.0")
+		  ebdb-vcard-default-40-formatter)
+		 (t (signal 'ebdb-unparseable-vcard
+			    (list (format
+				   "Unparseable vCard version: %s"
+				   version)))))))
+      (ebdb-parse-record fmt))))
+
+(defun ebdb-vcard-extract-property (&optional prop)
+  "Find and extract a single vcard property in the current
+  buffer.
+
+This can be used in one of two ways: with no optional PROP, it
+simply treats the current line as a vcard property and returns
+that, leaving point at the beginning of the next line.  With
+PROP, it searches for that property in particular, removes the
+line from the buffer altogether, then returns to the starting
+point.
+
+The return value is a three-element vector of [prop-name
+paramters value].  The parameters element is a list, each element
+of which is either a string, for parameters with no value, or a
+cons cell of (param-name . value).
+
+This function is also responsible for transforming or decoding
+values and parameters as necessary."
+  (let* ((line
+	  (if prop
+	      (save-excursion
+		(re-search-forward
+		 (concat "^" prop "[:;]") (point-max) t)
+		(delete-and-extract-region
+		 (point-at-bol)
+		 (progn (forward-line) (point-at-bol))))
+	    (buffer-substring-no-properties
+	     (point-at-bol)
+	     (progn (forward-line)
+		    (point)))))
+	 (parts (split-string line ":"))
+	 (val (mapconcat #'string-trim (cdr parts) ":"))
+	 (prop-and-param (split-string (car parts) ";"))
+	 (prop (car prop-and-param))
+	 (params (cdr prop-and-param)))
+    (when params
+      (setq params (ebdb-vcard-treat-params params)))
+    (vector prop params val)))
+
+(defun ebdb-vcard-treat-params (params)
+  "Handle a list of parameters.
+
+Return the treated list.  Single-term paramters are returned as a
+single string.  Key/value parameters are returned as a cons cell
+of (key . value)."
+  (mapcar
+   (lambda (el)
+     (pcase (split-string el "=")
+       (`(and ,el (stringp))
+	el)
+       (`(,param ,value)
+	(cons param value))))
+   params))
+
+(cl-defmethod ebdb-parse-record ((fmt ebdb-formatter-vcard-40))
+  "For v4.0, first use the KIND property to set record class."
+  ;; Do the name here!  And have another method that does the name for
+  ;; 3.0.  That way we can set it properly, and then the base method
+  ;; can just go about its business.
+  (let* ((kind (downcase
+		(cdr (ebdb-vcard-extract-property "kind"))))
+	 (record-class (cdr (assoc kind ebdb-vcard-kind-class-alist))))
+    (cl-call-next-method fmt record-class)))
+
+(cl-defmethod ebdb-parse-record ((fmt ebdb-formatter-vcard)
+				 &optional record-class fields)
+  "Parse the current buffer as vCard text, using FMT.
+
+FMT is an instance of `ebdb-formatter-vcard', meaning that at
+this point we should already have detected the vCard version, and
+chosen the correct formatter."
+  (let* ((record-class (or record-class ebdb-default-record-class))
+	 ;; Something needs refactoring here.  Probably records should
+	 ;; be allowed to parse their own names.
+	 (name-class (if (child-of-class-p record-class 'ebdb-record-person)
+			 'ebdb-field-name-complex
+		       'ebdb-field-name-simple))
+	 (name (ebdb-parse-vcard-field fmt name-class
+				       (aref (ebdb-vcard-extract-property "n") 2)))
+	 (record (make-instance record-class :name name))
+	 chunks unusable-fields field-class)
+    (goto-char (point-min))
+    (while (null (eobp))
+      (push (ebdb-vcard-extract-property) chunks))
+    ;; Handle role fields here.
+    (pcase-dolist (`[,label ,params ,value] chunks)
+      (setq field-class (cdr-safe (assoc-string label ebdb-vcard-label-alist)))
+      (or (and field-class
+	       (condition-case nil
+		   (push (ebdb-parse-vcard-field
+			  fmt field-class value params)
+			 fields)
+		 (ebdb-unparseable nil)))
+	  (push (vector label params value) unusable-fields)))
+    ;; We'll want to do more examining of unusable-fields than this,
+    ;; but for now just treat anything with an X- prefix as a simple
+    ;; user field.
+    (dolist (f unusable-fields)
+      (when (string-prefix-p "X-" (aref f 0))
+	(push (make-instance 'ebdb-field-user-simple
+			     :object-name (aref f 0)
+			     :value (aref f 2))
+	      fields)))
+    (dolist (f fields)
+      (condition-case nil
+	  (ebdb-record-insert-field record f)
+	(ebdb-unacceptable-field
+	 (push f unusable-fields))))
+    record))
+
+(cl-defgeneric ebdb-parse-vcard-field (fmt field-class string &optional params)
+  "Parse STRING as instance of FIELD-CLASS, as specified by FMT.
+
+PARAMS, if present, is a list of parameters.")
+
+(cl-defmethod ebdb-parse-vcard-field ((_fmt ebdb-formatter-vcard)
+				      (cls (subclass ebdb-field-labeled))
+				      (str string)
+				      &optional params)
+  (let ((inst (cl-call-next-method)))
+   (when-let ((label (cdr-safe (assoc-string "type" params))))
+     (setf (slot-value inst 'object-name) label))
+   inst))
+
+(cl-defmethod ebdb-parse-vcard-field ((_fmt ebdb-formatter-vcard)
+				      (cls (subclass ebdb-field-name-complex))
+				      (str string)
+				      &optional _params)
+  "Parse a vCard N property value."
+  
+  (pcase-let ((`(,surname ,given ,given-cont ,prefix ,suffix)
+	       (split-string str ";")))
+    (make-instance
+     cls
+     :surname surname
+     :given-names (cons given (split-string given-cont ","))
+     :prefix prefix
+     :suffix suffix)))
+
+(cl-defmethod ebdb-parse-vcard-field ((_fmt ebdb-formatter-vcard)
+				      (cls (subclass ebdb-field-address))
+				      (str string)
+				      &optional _params)
+  "Parse a vCard ADR property value."
+  (pcase-let ((`(,po ,ext ,streets ,local ,region ,pc ,country)
+	       (split-string str ";")))
+    (when po
+      (setf (car streets) (concat po (car streets))))
+    (when ext
+      (push ext streets))
+    (make-instance
+     cls
+     :streets streets
+     :locality local
+     :region region
+     :postcode pc
+     :country country)))
+
+(cl-defmethod ebdb-parse-vcard-field ((_fmt ebdb-formatter-vcard)
+				      (cls (subclass ebdb-field))
+				      (str string)
+				      &optional _params)
+  (ebdb-parse cls str))
 
 (provide 'ebdb-vcard)
 ;;; ebdb-vcard.el ends here
