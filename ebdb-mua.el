500229215654d82b2ccce36e4c7cc15f166f9632
Santiago Payà i Miralta <santiagopim@gmail.com>
2022-02-24 17:01:59 +0100

    Documentation variable names typos, around `ebdb-accept-header-alist'

--- ebdb-mua.el
+++ ebdb-mua.el
@@ -178,7 +178,7 @@ also consider the email addresses in the remaining headers."
 The format of this alist is
    ((HEADER-TYPE . REGEXP) ...)
 
-Where HEADER-TYPE is one of the symbols 'sender, 'recipients',
+Where HEADER-TYPE is one of the symbols 'sender, 'recipients,
 'any (meaning 'sender or 'recipients), or 'subject.
 
 For example, if
@@ -186,14 +186,14 @@ For example, if
     (subject . \"time travel\"))
 EBDB records are only created for messages sent by people at Maximegalon U.,
 or people posting about time travel.
-If t accept all messages.  If nil, accept all messages.
+If t accept all messages.  If nil, does not accept any message.
 
 See also `ebdb-ignore-header-alist', which has the opposite effect."
   :type '(repeat (cons
                   (choice (symbol :tag "Sender" sender)
-			  (symbol :tag "Recipients" recipients)
-			  (symbol :tag "Sender or recipients" any)
-			  (symbol :tag "Subject" subject))
+			              (symbol :tag "Recipients" recipients)
+			              (symbol :tag "Sender or recipients" any)
+			              (symbol :tag "Subject" subject))
                   (regexp :tag "Regexp to match on header value"))))
 
 (defcustom ebdb-ignore-header-alist nil
