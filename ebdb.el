333c68f7e95590e867ca02742c9f5d612e633c28
Eric Abrahamsen <eric@ericabrahamsen.net>
2022-03-20 17:07:01 -0700

    Release 0.8.13

--- ebdb.el
+++ ebdb.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2016-2021  Free Software Foundation, Inc.
 
-;; Version: 0.8.12
+;; Version: 0.8.13
 ;; Package-Requires: ((emacs "25.1") (seq "2.15"))
 
 ;; Maintainer: Eric Abrahamsen <eric@ericabrahamsen.net>
